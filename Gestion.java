6f0b58ace8bc0a30f5171d3da6d0965e0f1d4ded
Peztak <92151905+Peztak@users.noreply.github.com>
2022-11-14 15:35:44 +0100

    Add files via upload

---
+++ Gestion.java
@@ -0,0 +1,13 @@
+import java.awt.Dimension;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+import javax.swing.*;
+
+public class Gestion {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Gestion Stock");
+        f.setSize(1550, 800);
+        f.setVisible(true);
+    }
+}
